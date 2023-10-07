ab406054c6e6651564228cb9e84b83d539cb5cea
Jess <jessjelardo@gmail.com>
2023-10-08 00:58:49 +0800

    added remaining cards and score tracking functionality

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 let deckId
+let computerScore = 0
+let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
@@ -17,28 +25,37 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+            
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
+                if (computerScore > myScore) {
+                    // display "The computer won the game!"
+                    header.textContent = "The computer won the game!"
+                } else if (myScore > computerScore) {
+                    // display "You won the game!"
+                    header.textContent = "You won the game!"
+                } else {
+                    // display "It's a tie game!"
+                    header.textContent = "It's a tie game!"
+                }
+            }
         })
 })
+
 /**
  * Challenge:
  * 
- * Try to determine which of the 2 cards is the "winner" (has higher value)
- * Aces are the card with the highest "score"
- * 
- * Part 2:
- * Instead of logging the winner to the console, 
- * display an `h2` on the screen above the 2 cards 
- * that declares who the winner is.
- * 
- * If card1 is the higher card, display "Computer wins!"
- * If card2 is the higher card, display "You win!"
- * If they're equal, display "War!"
+ * Display the final winner in the header at the top by
+ * replacing the text of the h2.
  */
 
 function determineCardWinner(card1, card2) {
@@ -46,24 +63,17 @@ function determineCardWinner(card1, card2) {
     "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
-    console.log("card 1:", card1ValueIndex)
-    console.log("card 2:", card2ValueIndex)
     
     if (card1ValueIndex > card2ValueIndex) {
-        console.log("Card 1 wins!")
+        computerScore++
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
+        return "Computer wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        console.log("Card 2 wins!")
+        myScore++
+        myScoreEl.textContent = `My score: ${myScore}`
+        return "You win!"
     } else {
-        console.log("It's a tie!")
+        return "War!"
     }
 }
 
-// Keeping this test case for reference, but should delete later
-// const card1Obj = {
-//     value: "JACK"
-// }
-// const card2Obj = {
-//     value: "QUEEN"
-// }
-
-// determineCardWinner(card1Obj, card2Obj)
